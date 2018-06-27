4bbba332060a7fd0ec2f078133ed42c4302b9683
tssule3 <tssule3@gmail.com>
2018-06-27 09:08:37 +0530

    second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBE8MaPGsr_K06B33_hk0C2UlhbbitThVA",
+    authDomain: "blog-app-19caf.firebaseapp.com",
+    databaseURL: "https://blog-app-19caf.firebaseio.com",
+    projectId: "blog-app-19caf",
+    storageBucket: "blog-app-19caf.appspot.com",
+    messagingSenderId: "767520763733"
+  }
 };
 
 /*
